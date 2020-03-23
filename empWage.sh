b0a9d57bd1443ad85ab9623b544aba9b3560b825
dvranga <devangmranganth>
2020-03-23 16:38:52 +0530

    working hours ina month

--- empWage.sh
+++ empWage.sh
@@ -1,17 +1,28 @@
 #! /bin/bash -x
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-empCheck=$(( $RANDOM%3 ));
-case $empCheck in
-	$isFullTime)
+# CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+#VARIABLES
+totalEmpHr=0;
+totalWorkingDays=0;
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+	(( totalWorkingDays++ ))
+	empCheck=$(( $RANDOM%3 ));
+	case $empCheck in
+		$IS_FULL_TIME)
 		empHrs=8
-			;;
-	$isPartTime)
-		empHrs=8
-			;;
+		;;
+		$IS_PART_TIME)
+		empHrs=4
+		;;
 		*)
 		empHrs=0
-			;;
-esac
-salary=$(( $empHrs*$empRatePerHr ));
+		;;
+	esac
+	totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
+done
+totalSalary=$(( $totalEmpHrs*$EMP_RATE_PER_HR ));
