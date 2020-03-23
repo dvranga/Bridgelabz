9a9d79b50cefe58a61f6b74e7cd5755d9d9eba67
dvranga <devangmranganth>
2020-03-23 13:45:10 +0530

    salary for a month

--- empWage.sh
+++ empWage.sh
@@ -1,17 +1,23 @@
-#welcome to employee
-#working hours
 #! /bin/bash -x
-fullday=1;
-halfday=0;
-randomCheck=$(( $RANDOM%2 ));
-if [ $fullday -eq $randomCheck ]
-then 
-	empHrs=8;
-	echo $empHrs
-elif [ $halfday -eq $randomCheck ]
-then 
-	empHrs=4;
-	echo $empHrs
-else
-	echo employee hours =0
-fi
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=30;
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$(( $RANDOM%3 ))
+		case $empCheck in
+			$isFullTime)
+				empHrs=8
+				;;
+			$isPartTime)
+				empHrs=4
+				;;
+			*)
+			empHrs=0
+				;;
+	esac
+	salary=$(( $empHrs*$empRatePerHr ));
+	totalSalary=$(( $totalSalary+$salary ));
+done
