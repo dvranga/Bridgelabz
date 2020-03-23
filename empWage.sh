a024a9d01b7d8d7500181632dc8a35a312c45089
dvranga <devangmranganth>
2020-03-23 16:05:05 +0530

    part time is 8 hours

--- empWage.sh
+++ empWage.sh
@@ -1,11 +1,17 @@
 #! /bin/bash -x
-isPresent=1;
-randomCheck=$(( $RANDOM%2 ));
-if [ $isPresent -eq $randomCheck ]
-then 
-	empRatePerHr=20;
-	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr));
-else
-	salary=0;
-fi
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$(( $RANDOM%3 ));
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+			;;
+	$isPartTime)
+		empHrs=8
+			;;
+		*)
+		empHrs=0
+			;;
+esac
+salary=$(( $empHrs*$empRatePerHr ));
