f5631d16639766830539d5e4d28c732e8e150677
dvranga <devangmranganth>
2020-03-23 13:34:29 +0530

    welcome

--- empWage.sh
+++ empWage.sh
@@ -1,1 +1,17 @@
 #welcome to employee
+#working hours
+#! /bin/bash -x
+fullday=1;
+halfday=0;
+randomCheck=$(( $RANDOM%2 ));
+if [ $fullday -eq $randomCheck ]
+then 
+	empHrs=8;
+	echo $empHrs
+elif [ $halfday -eq $randomCheck ]
+then 
+	empHrs=4;
+	echo $empHrs
+else
+	echo employee hours =0
+fi
